d82a00b7cf195559115366b8b53a48c7c670c630
Enrico Cuono <105428037+Enrico258@users.noreply.github.com>
2025-04-11 08:57:24 -0300

    Update Potencia.java

--- src/main/java/Potencia.java
+++ src/main/java/Potencia.java
@@ -1,9 +1,9 @@
 public class Potencia {
     public int potencia(int a, int b) {
-        int resultado = 1;
+        int n = a;
         for (int i = 0; i < b-1; i++) {
-            resultado *= a;
+            a *= n;
         }
-        return resultado;
+        return a;
     }
 }
