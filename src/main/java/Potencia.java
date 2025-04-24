164f42d07e2acc5908ca67160c5e55deaa701529
Enrico Cuono <105428037+Enrico258@users.noreply.github.com>
2025-04-23 23:57:10 -0300

    Delete src directory

--- src/main/java/Potencia.java
+++
@@ -1,9 +0,0 @@
-public class Potencia {
-    public int potencia(int a, int b) {
-        int n = a;
-        for (int i = 0; i < b-1; i++) {
-            a *= n;
-        }
-        return a;
-    }
-}
